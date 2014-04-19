42dfe8bc24800e28e43416a9f778d4375df67424
cburbridge <cburbridge@gmail.com>
2014-04-19 23:12:55 +0100

    extra action stuff

--- strands_action_domain/scripts/test.py
+++ strands_action_domain/scripts/test.py
@@ -5,4 +5,11 @@ from  rospkg import rospack
 __rospack = rospack.RosPack()
 
 import strands_action_domain
-print strands_action_domain.STRANDSDomain().domain #generatePDDL()
+
+strands_domain =  strands_action_domain.STRANDSDomain()
+print strands_domain.domain #generatePDDL()
+
+# For example
+#task =  strands_domain.domain.get_action("launch").create_executive_task({'param1': 'scitos_bringup.launch',})
+# schedule / execute task...
+
